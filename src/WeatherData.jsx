5161532122b7e6988937c1638d923e8d0315e0f4
Emily Till <emileefrances@hotmail.co.uk>
2020-06-06 20:31:37 +0100

    added icons

--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { WiCloudy } from "react-icons/wi";
+import { WiThermometer } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
+import { WiSunrise } from "react-icons/wi";
+import { WiSunset } from "react-icons/wi";
+import { IoIosHome } from "react-icons/io";
+import { format } from "date-fns";
 import getWeatherData from "./core/getWeatherData";
 
 class WeatherData extends React.Component {
@@ -18,24 +25,50 @@ class WeatherData extends React.Component {
     });
   }
 
+  convertKelvinToCelsius(num) {
+    return Math.round((num - 273.15) * 100) / 100;
+  }
+
+  formatDate(date) {
+    return format(date, "HH:mm");
+  }
+
   render() {
     const data = this.state.data;
-
+    console.log(this);
     if (data === null) {
       return null;
     }
     console.log(data);
     return (
       <div>
-        <p>Location: {data.name}</p>
-        <p>Weather: {data.weather[0].main}</p>
-        <p>Temperature: {data.main.temp}</p>
         <p>
+          <IoIosHome />
+          Location: {data.name}
+        </p>
+        <p>
+          <WiCloudy />
+          Weather: {data.weather[0].main}
+        </p>
+
+        <p>
+          <WiThermometer />
+          Temperature: {this.convertKelvinToCelsius(data.main.temp)}
+        </p>
+        <p>
+          <WiStrongWind />
           Wind Speed: {data.wind.speed}
           <abbr title="meters per second"> m/sec</abbr>
         </p>
-        <p>Sunrise: {new Date(data.sys.sunrise).toISOString()}</p>
-        <p>Sunset: {new Date(data.sys.sunset).toISOString()}</p>
+
+        <p>
+          <WiSunrise />
+          Sunrise: {this.formatDate(new Date(data.sys.sunrise))}
+        </p>
+        <p>
+          <WiSunset />
+          Sunset: {this.formatDate(new Date(data.sys.sunset))}
+        </p>
       </div>
     );
   }
--- src/core/getWeatherData.js
+++ src/core/getWeatherData.js
@@ -2,13 +2,9 @@ var url =
   "http://api.openweathermap.org/data/2.5/weather?q=leicester&appid=c4f320d668e5ec15a88bf95db5d697b1&q";
 
 function getWeatherData() {
-  return fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return data;
-    });
+  return fetch(url).then((response) => {
+    return response.json();
+  });
 }
 
 export default getWeatherData;
